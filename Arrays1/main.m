c910b6337727fee062a01e998b5f8b3852bbaa39
praveen <praveen@gmail.com>
2017-02-16 18:24:59 -0500

    Initial Commit

---
+++ Arrays1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Arrays1
+//
+//  Created by Praveen on 2017-02-16.
+//  Copyright © 2017 Praveen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
